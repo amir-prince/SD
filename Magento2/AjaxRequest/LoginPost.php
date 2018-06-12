a8b4abc7dcd1b44d47a881405cad768163a88c2b
Prince <amirposka@gmail.com>
2018-06-12 11:45:21 +0200

    Add content LoginPost.php

--- Magento2/AjaxRequest/LoginPost.php
+++ Magento2/AjaxRequest/LoginPost.php
@@ -1,1 +1,212 @@
-gg
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Sd\Ajaxlogin\Controller\Account;
+
+use Magento\Customer\Model\Account\Redirect as AccountRedirect;
+use Magento\Framework\App\Action\Context;
+use Magento\Customer\Model\Session;
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Model\Url as CustomerUrl;
+use Magento\Framework\Exception\EmailNotConfirmedException;
+use Magento\Framework\Exception\AuthenticationException;
+use Magento\Framework\Data\Form\FormKey\Validator;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\State\UserLockedException;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class LoginPost extends \Magento\Customer\Controller\AbstractAccount
+{
+    /**
+     * @var \Magento\Customer\Api\AccountManagementInterface
+     */
+    protected $customerAccountManagement;
+
+    /**
+     * @var \Magento\Framework\Data\Form\FormKey\Validator
+     */
+    protected $formKeyValidator;
+
+    /**
+     * @var AccountRedirect
+     */
+    protected $accountRedirect;
+
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory
+     */
+    private $cookieMetadataFactory;
+
+    /**
+     * @var \Magento\Framework\Stdlib\Cookie\PhpCookieManager
+     */
+    private $cookieMetadataManager;
+
+    protected $resultFactory;
+
+    /**
+     * @param Context $context
+     * @param Session $customerSession
+     * @param AccountManagementInterface $customerAccountManagement
+     * @param CustomerUrl $customerHelperData
+     * @param Validator $formKeyValidator
+     * @param AccountRedirect $accountRedirect
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        AccountManagementInterface $customerAccountManagement,
+        CustomerUrl $customerHelperData,
+        Validator $formKeyValidator,
+        AccountRedirect $accountRedirect,
+        ResultFactory $resultFactory
+    ) {
+        $this->session = $customerSession;
+        $this->customerAccountManagement = $customerAccountManagement;
+        $this->customerUrl = $customerHelperData;
+        $this->formKeyValidator = $formKeyValidator;
+        $this->accountRedirect = $accountRedirect;
+        $this->resultFactory = $resultFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Get scope config
+     *
+     * @return ScopeConfigInterface
+     * @deprecated 100.0.10
+     */
+    private function getScopeConfig()
+    {
+        if (!($this->scopeConfig instanceof \Magento\Framework\App\Config\ScopeConfigInterface)) {
+            return \Magento\Framework\App\ObjectManager::getInstance()->get(
+                \Magento\Framework\App\Config\ScopeConfigInterface::class
+            );
+        } else {
+            return $this->scopeConfig;
+        }
+    }
+
+    /**
+     * Retrieve cookie manager
+     *
+     * @deprecated 100.1.0
+     * @return \Magento\Framework\Stdlib\Cookie\PhpCookieManager
+     */
+    private function getCookieManager()
+    {
+        if (!$this->cookieMetadataManager) {
+            $this->cookieMetadataManager = \Magento\Framework\App\ObjectManager::getInstance()->get(
+                \Magento\Framework\Stdlib\Cookie\PhpCookieManager::class
+            );
+        }
+        return $this->cookieMetadataManager;
+    }
+
+    /**
+     * Retrieve cookie metadata factory
+     *
+     * @deprecated 100.1.0
+     * @return \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory
+     */
+    private function getCookieMetadataFactory()
+    {
+        if (!$this->cookieMetadataFactory) {
+            $this->cookieMetadataFactory = \Magento\Framework\App\ObjectManager::getInstance()->get(
+                \Magento\Framework\Stdlib\Cookie\CookieMetadataFactory::class
+            );
+        }
+        return $this->cookieMetadataFactory;
+    }
+
+    /**
+     * Login post action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    public function execute()
+    {
+        if ($this->session->isLoggedIn() || !$this->formKeyValidator->validate($this->getRequest())) {
+            /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $resultRedirect->setPath('*/*/');
+            return $resultRedirect;
+        }
+
+        if ($this->getRequest()->isPost()) {
+            $login = $this->getRequest()->getPost('login');
+            if (!empty($login['username']) && !empty($login['password'])) {
+                try {
+                    $customer = $this->customerAccountManagement->authenticate($login['username'], $login['password']);
+                    $this->session->setCustomerDataAsLoggedIn($customer);
+                    $this->session->regenerateId();
+                    if ($this->getCookieManager()->getCookie('mage-cache-sessid')) {
+                        $metadata = $this->getCookieMetadataFactory()->createCookieMetadata();
+                        $metadata->setPath('/');
+                        $this->getCookieManager()->deleteCookie('mage-cache-sessid', $metadata);
+                        /*Ajex request here*/
+                        $response ='200';
+                        $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+                        $resultJson->setData($response);
+                        return $resultJson;
+                    }
+                    $redirectUrl = $this->accountRedirect->getRedirectCookie();
+                    if (!$this->getScopeConfig()->getValue('customer/startup/redirect_dashboard') && $redirectUrl) {
+                        $this->accountRedirect->clearRedirectCookie();
+                        return $this->_redirect($this->_redirect->getRefererUrl());
+                    }
+                } catch (EmailNotConfirmedException $e) {
+                    $value = $this->customerUrl->getEmailConfirmationUrl($login['username']);
+                    $message = __(
+                        'This account is not confirmed. <a href="%1">Click here</a> to resend confirmation email.',
+                        $value
+                    );
+                    $this->messageManager->addError($message);
+                    echo $message.'&nbsp'.'<i class="fas fa-exclamation-circle"></i>';
+                    $this->session->setUsername($login['username']);
+                } catch (UserLockedException $e) {
+                    $message = __(
+                        'You did not sign in correctly or your account is temporarily disabled.'
+                    );
+                    $this->messageManager->addError($message);
+                    echo $message.'&nbsp'.'<i class="fas fa-exclamation-circle"></i>';
+                    $this->session->setUsername($login['username']);
+                } catch (AuthenticationException $e) {
+                    $message = __('Identifiant ou mot de passe incorrect');
+                    $this->messageManager->addError($message);
+                    echo $message.'&nbsp'.'<i class="fas fa-exclamation-circle"></i>';
+                    $this->session->setUsername($login['username']);
+                } catch (LocalizedException $e) {
+                    $message = $e->getMessage();
+                    $this->messageManager->addError($message);
+                    echo $message.'&nbsp'.'<i class="fas fa-exclamation-circle"></i>';
+                    $this->session->setUsername($login['username']);
+                } catch (\Exception $e) {
+                    // PA DSS violation: throwing or logging an exception here can disclose customer password
+                    echo __('Une erreur inconnue est survenue. Veuillez nous contacter pour obtenir de l\'aide.');
+
+                }
+            } else {
+                echo __('L\'email et le mot de passe sont requis ').'&nbsp'.'<i class="fas fa-exclamation-circle"></i>';
+            }
+        }
+        //return $this->_redirect($this->_redirect->getRefererUrl());
+    }
+}
